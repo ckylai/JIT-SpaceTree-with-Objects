d7874221d2beb0c5292e92be0945df400d4e8313
Carrie Lai <carrie_k_y_lai@hotmail.com>
2011-10-05 15:36:06 -0700

    set up rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JITSpaceTreeWithObjects::Application.config.secret_token = '78383041171b86788362d8ff7fc96345fcc93f531a6c6d2ff8b285b37123403d85434fb9b9a3c89c59a262537beffdcc3de00b810a650d34390901180dba29ef'
